fc859f718938ae1c9d8695b354eb4c0aa4a4c28d
Alexandre <megalexandre@gmail.com>
2026-02-19 21:05:47 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/deploy-projects-app/',
+  base: process.env.NODE_ENV === 'production' ? '/deploy-projects-app/' : './',
   plugins: [react()],
   build: {
     outDir: 'dist',
